5d66a37823f79bad120d3f7eebff526f41ea8378
Scott Messner <blackstrype@gmail.com>
2026-07-26 23:13:08 +0200

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        int left_product = 1;
+        int i = 0;
+        // Populate output with the product of all elements to the left of i
+        for(; i < nums.length; i++) {
+            output[i] = left_product;
+            left_product *= nums[i];
+        }
+        int right_product = 1;
+        // Populate output with the product of all elements to the right and left of i
+        for(; i > 0; i--) {
+            output[i-1] *= right_product;
+            right_product *= nums[i-1];
+        }
+        return output;
+    }
+}  
